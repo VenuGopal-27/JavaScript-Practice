549504ff6565c2d4ff2174e7cf780e889e33def2
MAMIDI VENU GOPAL <141650397+VenuGopal-27@users.noreply.github.com>
2025-04-07 09:36:45 +0000

    Data Types

---
+++ 01_basics/2_datatypes.js
@@ -0,0 +1,15 @@
+let age = 21 // number
+
+let fName = "Venu Gopal" //String
+
+let isSleeping = true // boolean
+
+let sym = Symbol("unique") // Symbol
+
+let y = null // null
+
+let arr = [1,2,3,4,5] // array
+
+let obj = {name:"Venu Gopal",age:21, address:"Odisha"} // Object
+
+console.table([age, fName, isSleeping, sym, y, arr, obj])
