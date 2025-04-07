763c5429b17dcb7590a60ae33d6c007e190a0ca5
MAMIDI VENU GOPAL <141650397+VenuGopal-27@users.noreply.github.com>
2025-04-07 09:51:49 +0000

    conversion of data types

---
+++ 01_basics/3_Conversion.js
@@ -0,0 +1,11 @@
+let age = true;
+
+console.log(typeof age)
+
+let convertedAge = Number(age)
+console.log(typeof convertedAge)
+console.log(convertedAge)
+
+convertedAge = Boolean(convertedAge)
+console.log(typeof convertedAge)
+console.log(convertedAge)
