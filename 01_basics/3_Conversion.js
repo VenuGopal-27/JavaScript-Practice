02458c65cfe31af35e94f8b9244b366f5e140e9b
MAMIDI VENU GOPAL <141650397+VenuGopal-27@users.noreply.github.com>
2025-04-08 05:24:02 +0000

    Comparisons and Conversions

--- 01_basics/3_Conversion.js
+++ 01_basics/3_Conversion.js
@@ -1,11 +1,17 @@
-let age = true;
+// let age = true;
 
-console.log(typeof age)
+// console.log(typeof age)
 
-let convertedAge = Number(age)
-console.log(typeof convertedAge)
-console.log(convertedAge)
+// let convertedAge = Number(age)
+// console.log(typeof convertedAge)
+// console.log(convertedAge)
 
-convertedAge = Boolean(convertedAge)
-console.log(typeof convertedAge)
-console.log(convertedAge)
+// convertedAge = Boolean(convertedAge)
+// console.log(typeof convertedAge)
+// console.log(convertedAge)
+
+let str = 57
+let str2 = 1
+let num = "3"
+
+console.log(str+str2+num);
---
+++ 01_basics/4Comarisons.js
@@ -0,0 +1,15 @@
+// console.log(2>1)
+
+// console.log(null>0)
+// console.log(null == 0);
+// console.log(null>=0);
+
+// console.log(NaN != NaN)
+
+//NOTE:
+// Equality check (== or ===) are different from comparison operations(> < >= <=)
+// == is just an equality check whereas === is strict check which means it even checks for the data type, consider below example for better understanding
+
+console.log("2" == 2)
+
+console.log("2"===2)
